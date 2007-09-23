2e536174ef9bcf7ad075f0bd5fa8e91a52f10623
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-09-23 18:38:58 +0000

    Localisation updates from Betawiki. * Fixes and additions to 32 extensions for ar, hsb, la, nl, sk, tet.

--- Gadgets.i18n.hsb.php
+++ Gadgets.i18n.hsb.php
@@ -6,10 +6,19 @@
  * @package MediaWiki
  * @subpackage Extensions
  * @author Daniel Kinzler, brightbyte.de
- * @copyright Â© 2007 Daniel Kinzler, Betawiki users
+ * @copyright © 2007 Daniel Kinzler, Betawiki users
  * @licence GNU General Public Licence 2.0 or later
  */
 
 $messages = array(
+	'gadgets-prefs' => 'Specialne funkcije',
+	'gadgets-prefstext' => 'Deleka je lisćina specialnych funkcijow, kotrež móžeš za swoje wužiwarske konto aktiwizować. Tute specialne funkcije zwjetša na JavaScripće bazěruja, tohodla dyrbi JavaScript we wobhladowaku aktiwizowany być, zo bychu fungowali.
+Wobkedźbuj tež, zo so tute specialne funkcije na stronje z twojimi wosobinskimi nastajenjemi njewuskutkuja.
+
+Nimo toho wobkedźbuj, zo tute specialne funkcije nejsu dźěl softwary MediaWiki a so zwjetša wot wužiwarjow na swojim lokalnym wikiju wuwiwaja a wothladuja. Lokalni administratorojo móža lisćinu k dispoziciji stejacych specialnych funkcijow přez [[MediaWiki:Gadgets-definition]] a [[Special:Gadgets]] wobdźěłać.',
+	'gadgets' => 'Specialne funkcije',
+	'gadgets-title' => 'Specialne funkcije',
+	'gadgets-pagetext' => 'Deleka je lisćina specialnych funkcijow, kotrež wwužiwarjo móža na swojej stronje nastajenjow, kaž přez [[MediaWiki:Gadgets-definition]] definowane.
+Tutón přehlad skići lochki přistup k systemowym zdźělenkam, kotrež pomjenjowanje a kod kóždeje specialneje funkcije wobsahuja.',
 	'gadgets-uses' => 'Wužiwa',
 );
