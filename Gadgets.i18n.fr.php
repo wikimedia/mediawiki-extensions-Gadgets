0c04b8b07312922017ed2b478506b89c9e1ebcbb
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-04 20:08:37 +0000

    Localisation updates from Betawiki. * Fixes and additions to 34 extensions for fr * export format for ContributorsMessageGroup, FancyCaptchaMessageGroup, MediaFunctionsMessageGroup, PicturePopupMessageGroup, ProfileMonitorMessageGroup changed * indentation for Contributors.i18n.php, FancyCaptcha.i18n.php, MediaFunctions.i18n.php, PicturePopup.i18n.php, ProfileMonitor.i18n.php changed

---
+++ Gadgets.i18n.fr.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'gadgets-prefstext' => 'Ci-dessous apparaît une liste de gadgets que vous pouvez activer pour votre compte. Ils font appel à [[JavaScript]], il doit donc être activé pour votre navigateur Web.
+Ils n\'ont aucune incidence sur cette page de préférences. De plus, ils sont généralement développés et maintenus sur ce wiki. Les administrateurs peuvent modifier les gadgets en passant par [[MediaWiki:Gadgets-definition]] et [[Special:Gadgets]].',
+	'gadgets-pagetext' => 'Ci-dessous apparaît une liste de gadgets que les utilisateurs peuvent activer dans leur page de préférences, tel que défini dans \'\'[[MediaWiki:Gadgets-definition]]\'\'. Ce survol donne un accès rapide aux pages de messages système qui définissent chaque description et chaque code des gadgets.',
+	'gadgets-uses' => 'Utilise',
+);
