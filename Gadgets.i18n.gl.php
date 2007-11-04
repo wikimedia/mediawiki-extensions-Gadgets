61d30c4330e553d2f025286f13730d52abc20a86
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-04 21:21:43 +0000

    Localisation updates from Betawiki. * Fixes and additions to 40 extensions for gl, hsb, id, is, it, ja, kk-kz, kk-tr, nl, no, oc * export format in extension Translate and indentation changed in: GiveRollback, ImageMap, Newuserlog, OggHandler, Oversight, ParserFunctions, ProofreadPage, Resign/SpecialResign

---
+++ Gadgets.i18n.gl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'gadgets-uses' => 'Usos',
+);
