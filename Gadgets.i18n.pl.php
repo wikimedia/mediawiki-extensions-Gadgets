937e0af3a9eea90d6e8e35a108d58cecb7f96141
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-05 00:05:07 +0000

    Localisation updates from Betawiki. * Fixes and additions to 45 extensions for pl, pms, pt, ro, sk, sr-ec, sr-el, su, sv, tet, tr, vi, wa, yue, zh-hans, zh-hant, zh-hk * export format in extension Translate and indentation changed in a few i18n files (BookInformation, Patroller, SpamBlacklist, SyntaxHighlight_GeSHi, UsernameBlacklist, WebStore)

--- Gadgets.i18n.pl.php
+++ Gadgets.i18n.pl.php
@@ -11,6 +11,8 @@
  */
 
 $messages = array(
+	'gadgets-prefs' => 'Gadżety',
+	'gadgets-prefstext' => 'Poniżej znajduje się lista specjalnych gadżetów, które możesz włączyć dla swojego konta. Te gadżety opierają się w większości na JavaScripcie, więc by działały musisz mieć w swojej przeglądarce włączony JavaScript. Pamiętaj, że gadżety nie będą miały wpływu na tę stronę preferencji. Oprócz tego, nie są one częścią oprogramowania MediaWiki i zazwyczaj są tworzone przez użytkowników tego wiki. Lokalni administratorzy mogą edytować dostępne gadżety używając stron [[MediaWiki:Gadgets-definition]] i [[Special:Gadgets]].',
 	'gadgets' => 'Gadżety',
 	'gadgets-title' => 'Gadżety',
 	'gadgets-pagetext' => 'Poniżej znajduje się lista specjalnych gadżetów, które użytkownicy mogą włączyć na swojej stronie preferencji, jak zdefiniowano to na stronie [[MediaWiki:Gadgets-definition]]. Ten przegląd daje łatwy dostęp do komunikatów systemu, które definiują opis i kod każdego z gadżetów.',
