9f92bd73a1578e8ec189d8516b148d1b7c5d811f
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-10 18:15:02 +0000

    Localisation updates from Betawiki. * Fixes and additions to 52 extensions for ar, cs, es, fi, frp, hsb, hu, la, nl, oc, pl, sk, tet * change indentation for export of a few extension's messages in Translate/MessageGroups.php

---
+++ Gadgets.i18n.oc.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'gadgets-prefstext' => 'Çai jos apareis una lista de gadgets que podètz activar per vòstre compte. Fan apèl a [[JavaScript]], deu doncas èsser activat per vòstre navegaire Web. An pas cap d\'incidéncia sus aquesta pagina de preferéncias. E mai, son generalament desvolopats e mantenguts sus aqueste wiki. Los administrators pòdon modificar los gadgets en passant per [[MediaWiki:Gadgets-definition]] e [[Special:Gadgets]].',
+	'gadgets-pagetext' => 'Çai jos apareis una lista de gadgets que los utilizaires pòdon activar dins lor pagina de preferéncias, coma definit dins \'\'[[MediaWiki:Gadgets-definition]]\'\'. Aqueste susvòl dona un accès rapid a las paginas de messatges del sistèma que definisson cada descripcion e cada còde dels gadgets.',
+	'gadgets-uses' => 'Utiliza',
+);
