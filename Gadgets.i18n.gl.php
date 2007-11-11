31c8013387a73473d1f2a118b259e595ffe5187a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-11 14:23:02 +0000

    Localisation updates from Betawiki. * Fixes and additions to 23 extensions for gl, hsb, it, kab, kk-kz, la, lv, no

--- Gadgets.i18n.gl.php
+++ Gadgets.i18n.gl.php
@@ -12,6 +12,12 @@
 
 $messages = array(
 	'gadgets-prefs' => 'Trebellos',
+	'gadgets-prefstext' => 'Embaixo hai unha lista de trebellos especiais que pode activar para a súa conta.
+A maioría destes trebellos baséanse en JavaScript, así que ten que ter o JavaScript activado no seu navegador para que funcionen.
+Teña en conta que estes trebellos non funcionarán nesta páxina de preferencias.
+
+Teña tamén en conta que estes trebellos especiais non son parte do software de MediaWiki e que os crean e manteñen
+os usuarios no seu wiki local. Os administradores locais poden editar os trebellos disponíbeis mediante [[MediaWiki:Gadgets-definition]] e [[Special:Gadgets]].',
 	'gadgets' => 'Trebellos',
 	'gadgets-title' => 'Trebellos',
 	'gadgets-uses' => 'Usos',
