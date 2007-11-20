29b55f4253e28e7cd17e68a3b8abced45638110c
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-11-20 22:48:36 +0000

    Localisation updates from Betawiki. * Fixes and additions to 18 extensions for be, el, fo, fr, nl, pl

---
+++ Gadgets.i18n.el.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'gadgets-prefs' => 'Συσκευές',
+	'gadgets' => 'Συσκευές',
+	'gadgets-title' => 'Συσκευές',
+);
--- Gadgets.i18n.fr.php
+++ Gadgets.i18n.fr.php
@@ -13,6 +13,6 @@
 $messages = array(
 	'gadgets-prefstext' => 'Ci-dessous apparaît une liste de gadgets que vous pouvez activer pour votre compte. Ils font appel à [[JavaScript]], il doit donc être activé pour votre navigateur Web.
 Ils n\'ont aucune incidence sur cette page de préférences. De plus, ils sont généralement développés et maintenus sur ce wiki. Les administrateurs peuvent modifier les gadgets en passant par [[MediaWiki:Gadgets-definition]] et [[Special:Gadgets]].',
-	'gadgets-pagetext' => 'Ci-dessous apparaît une liste de gadgets que les utilisateurs peuvent activer dans leur page de préférences, tel que défini dans \'\'[[MediaWiki:Gadgets-definition]]\'\'. Ce survol donne un accès rapide aux pages de messages système qui définissent chaque description et chaque code des gadgets.',
+	'gadgets-pagetext' => 'Ci-dessous apparaît une liste de gadgets que les utilisateurs peuvent activer dans leur [[Special:Preferences|page de préférences]], tel que défini dans \'\'[[MediaWiki:Gadgets-definition]]\'\'. Ce survol donne un accès rapide aux pages de messages système qui définissent chaque description et chaque code des gadgets.',
 	'gadgets-uses' => 'Utilise',
 );
