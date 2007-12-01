553727d5ac40d67e2e9b07826e0d976e54abe29a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-01 14:44:57 +0000

    Localisation updates from Betawiki. * Fixes and additions to 19 extensions for an, ar, arc, es, fr, hr, myv, nl, pt

---
+++ Gadgets.i18n.hr.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'gadgets-prefs' => 'JS ekstenzije (\'\'gadgets\'\')',
+	'gadgets-prefstext' => 'Slijedi popis posebnih ekstenzija koje možete omogućiti.
+One su većinom napisane u JavaScriptu, stoga JavaScript mora biti omogućen u vašem web-pregledniku da bi ektenzije radile.
+Nijedna ektenzija nema učinka na stranicu s postavkama.
+
+Ove posebne ekstenzije nisu dio MediaWiki softvera, najčešće su razvijane i održavane od suradnika na lokalnom wikiju.
+Lokalni administratori mogu uređivati dostupne ekstenzije putem [[MediaWiki:Gadgets-definition]] i [[Special:Gadgets]].',
+	'gadgets' => 'JS ekstenzije (\'\'gadgets\'\')',
+	'gadgets-title' => 'JS ekstenzije (\'\'gadgets\'\')',
+	'gadgets-pagetext' => 'Slijedi popis posebnih JavaScript ekstenzija koje suradnici mogu omogućiti u svojim postavkama, kako je definirano stranicom [[MediaWiki:Gadgets-definition]].
+Ovaj pregled omogućuje lak pristup porukama sustava koje opisuju ekstenzije i njihov kod.',
+	'gadgets-uses' => 'Koristi',
+);
