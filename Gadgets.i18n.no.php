d40c2c901f21b2bf18582088c470678b3191e28d
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-03 19:31:20 +0000

    Localisation updates from Betawiki. * Fixes and additions to 33 extensions for ar, el, fr, hr, hsb, nl, no, sk, yue, zh-hans, zh-hant

---
+++ Gadgets.i18n.no.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'gadgets-prefs' => 'Tilleggsutstyr',
+	'gadgets-prefstext' => 'Under er en liste over tilleggsutstyr du kan slå på for kontoen din.
+Dette tilleggsutstyret er for det meste basert på JavaScript, så du må ha dette slått på i nettleseren din for at de skal fungere.
+Merk at tilleggsutstyret ikke val ha noen innvirkning på denne innstillingssiden.
+
+Merk også at disse verktøyene ikke er del av MediaWiki-programvaren, og vanligvis utvikles og vedlikeholdes av brukere på den lokale wikien. Lokale administratorer kan redigere tilgjengelig verktøy ved å bruke [[MediaWiki:Gadgets-definition]] og [[Special:Gadgets]].',
+	'gadgets' => 'Tilleggsutstyr',
+	'gadgets-title' => 'Tilleggsutstyr',
+	'gadgets-pagetext' => 'Under er en liste over tilleggsutstyr brukere kan slå på på sin innstillingsside, som definert av [[MediaWiki:Gadgets-definition]]. Denne oversikten gir lett tilgang til systembeskjedsidene som definerer hvert verktøys beskrivelse og kode.',
+	'gadgets-uses' => 'Bruk',
+);
