356175acdeb713cea85d1384000b69d5cd75c3d0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-05 21:54:39 +0000

    Localisation updates from Betawiki. * Fixes and additions to 6 extensions for ar, hsb, nl, no, pt, stq

--- Gadgets.i18n.nl.php
+++ Gadgets.i18n.nl.php
@@ -10,19 +10,15 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
-#for Special:Preferences
-$messages['gadgets-prefs']= 'Gadgets';
-$messages['gadgets-prefstext']= 'Hieronder staat de speciale gadgets die u voor uw gebruiker kunt inschakelen.
+$messages = array(
+	'gadgets-prefstext' => 'Hieronder staan de speciale gadgets die u voor uw gebruiker kunt inschakelen.
 De gadgets zijn voornamelijk gebaseerd op JavaScript, dus JavaScript moet voor uw browser zijn ingeschakeld om ze te laten werken.
-De gadget hebben geen invloed op deze pagina met voorkeuren.
+De gadgets hebben geen invloed op deze pagina met voorkeuren.
 
 Deze speciale gadgets zijn ook geen onderdeel van de MediaWiki-software, en ze worden meestal ontwikkeld en onderhouden
 door gebruikers van uw lokale wiki. Lokale beheerders kunnen de beschikbare gadgets aangeven in [[MediaWiki:Gadgets-definition]] 
-en [[Special:Gadgets]].';
-
-#for Special:Gadgets
-$messages['gadgets'] = 'Gadgets';
-$messages['gadgets-title'] = 'Gadgets';
-$messages['gadgets-pagetext'] = 'Hieronder staan de speciale gadgets die gebruikers kunnen inschakelen via hun voorkeuren, zoals ingesteld in [[MediaWiki:Gadgets-definition]].
-Dit overzicht biedt eenvoudige toegang tot de systeemtekstpagina waar de beschrijving en de code van ieder gadget staat.';
-$messages['gadgets-uses'] = 'Gebruik';
+en [[Special:Gadgets]].',
+	'gadgets-pagetext' => 'Hieronder staan de speciale gadgets die gebruikers kunnen inschakelen via hun voorkeuren, zoals ingesteld in [[MediaWiki:Gadgets-definition]].
+Dit overzicht biedt eenvoudige toegang tot de systeemtekstpagina waar de beschrijving en de code van ieder gadget staat.',
+	'gadgets-uses' => 'Gebruik',
+);
