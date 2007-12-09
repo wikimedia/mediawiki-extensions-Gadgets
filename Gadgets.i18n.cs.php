0f7f8f1940a0915c79137f8bccdcf02251b9d814
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-09 07:58:14 +0000

    Localisation updates from Betawiki. * Fixes and additions to 13 extensions for bg, cs, hsb, hr, nl, ro

---
+++ Gadgets.i18n.cs.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'gadgets-prefs' => 'Udělátka',
+	'gadgets-prefstext' => 'Níže je přehled speciálních udělátek, které si můžete ve svém účtu zapnout.
+Tato udělátka jsou převážně založena na JavaScriptu, takže pro jejich funkčnost je nutné mít ve svém prohlížeči JavaScript zapnutý. 
+Udělátka nejsou aplikována na této stránce nastavení. 
+
+Uvědomte si také, že speciální udělátka nejsou součástí softwaru MediaWiki a&nbsp;jsou vytvářena a&nbsp;spravována uživateli této wiki. Místní správci mohou editovat udělátka prostřednictvím [[MediaWiki:Gadgets-definition]] a&nbsp;[[Special:Gadgets]].',
+	'gadgets' => 'Udělátka',
+	'gadgets-title' => 'Udělátka',
+	'gadgets-pagetext' => 'Níže je přehled speciálních udělátek, která si uživatelé mohou zapnout ve svém nastavení. Seznam lze upravovat na [[MediaWiki:Gadgets-definition]].
+Tento přehled poskytuje jednoduchý přístup k&nbsp;systémovým hlášením, která definují zdrojový kód a&nbsp;popis každého udělátka.',
+	'gadgets-uses' => 'používá',
+);
