39d68d8a30ba985f68e6ccda46f2eac99cc59e59
Rotem Liss <rotem@users.mediawiki.org>
2007-12-10 10:48:03 +0000

    Adding Hebrew translation.

---
+++ Gadgets.i18n.he.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Hebrew internationalisation file for the Gadgets extension
+ *
+ * @addtogroup Extensions
+ */
+
+# For Special:Preferences
+$messages['gadgets-prefs'] = 'סקריפטים';
+$messages['gadgets-prefstext'] = 'להלן רשימה של סקריפטים שתוכלו להתקין בחשבון שלכם.
+הסקריפטים מבוססים ברובם על שפת JavaScript, ולכן יש לאפשר אותה בדפדפן כדי שהם יעבדו.
+שימו לב שלא תהיה לסקריפטים כל השפעה על דף ההעדפות הזה.
+
+כמו כן, הסקריפטים אינם חלק מתוכנת מדיה־ויקי, והם בדרך כלל מפותחים ומתוחזקים על ידי משתמשים
+באתר זה. מפעילי המערכת יכולים לערוך את רשימת הסקריפטים האפשריים תוך שימוש ב[[MediaWiki:Gadgets-definition|הודעת המערכת]] ו[[Special:Gadgets|הדף המיוחד]] המתאימים.';
+
+# For Special:Gadgets
+$messages['gadgets'] = 'סקריפטים';
+$messages['gadgets-title'] = 'סקריפטים';
+$messages['gadgets-pagetext'] = 'זוהי רשימה של סקריפטים שמשתמשים יכולים להתקין באמצעות דף ההעדפות שלהם, כפי שהוגדרו ב[[MediaWiki:Gadgets-definition|הודעת המערכת המתאימה]].
+מכאן ניתן לגשת בקלות לדפי הודעות המערכת שמגדירים את התיאור והקוד של כל סקריפט.';
+$messages['gadgets-uses'] = 'משתמש בדפים';
