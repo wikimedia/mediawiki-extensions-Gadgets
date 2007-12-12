b48141824aeb6d404c6318e910cb2a4de35cc8cb
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-12 22:43:50 +0000

    Localisation updates from Betawiki. * Fixes and additions to 41 extensions for bg, ca, cs, el, gl, hsb, hu, nl, stq

---
+++ Gadgets.i18n.ca.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'gadgets-prefstext' => 'A continuació teniu una llista de «gadgets» especials que podeu activar al vostre compte.
+La majoria d\'aquests gadgets fan servir JavaScript, per tant haureu de tenir un navegador que funcioni amb aquest llenguatge activat per a que vos funcionin.
+Tingueu en compte que els gadgets no funcionaran en aquesta pàgina.
+
+També cal que tingueu present que aquests gadgets especials no formen part del programa MediaWiki i que acostumen a estar fets i mantinguts per usuaris del wiki local. Els administradors del wiki poden editar els gadgets disponibles a [[Special:Gadgets]].',
+	'gadgets-pagetext' => 'A continuació teniu una llista de «gadgets» especials que qualsevol usuari pot activar a la seva pàgina de preferències i que estan definits per [[MediaWiki:Gadgets-definition]].
+Aquesta llista us permet un fàcil accés a les pàgines del sistema que defineixen el codi de cada gadget.',
+	'gadgets-uses' => 'Usa',
+);
---
+++ Gadgets.i18n.hu.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+$messages = array(
+	'gadgets-prefs' => 'Kütyük',
+);
