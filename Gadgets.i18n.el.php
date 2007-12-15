84563859d1fd90cb7eda0e5e2ea70e8835b09f32
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-15 13:48:18 +0000

    Localisation updates from Betawiki. * Fixes and additions to 37 extensions for ar, bg, ca, diq, el, fr, hsb, hu, loz, lt, nl, no, oc, stq

--- Gadgets.i18n.el.php
+++ Gadgets.i18n.el.php
@@ -10,9 +10,15 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
+/** Greek (Ελληνικά)
+ * @author Consta
+ */
 $messages = array(
-	'gadgets-prefs' => 'Συσκευές',
-	'gadgets' => 'Συσκευές',
-	'gadgets-title' => 'Συσκευές',
-	'gadgets-uses' => 'Χρήσεις',
+	'gadgets-prefs'    => 'Συσκευές',
+	'gadgets'          => 'Συσκευές',
+	'gadgets-title'    => 'Συσκευές',
+	'gadgets-pagetext' => 'Παρακάτω βρίσκεται μια λίστα με τις ειδικές συσκευές χρηστών που επιτρέπονται στη σελίδα προτιμήσεών σας, όπως καθορίζεται από το [[MediaWiki:Gadgets-definition]].
+Αυτή η επισκόπηση παρέχει εύκολη πρόσβαση στις σελίδες μηνυμάτων του συστήματος που καθορίζουν την περιγραφή και τον κώδικα κάθε συσκευής.',
+	'gadgets-uses'     => 'Χρήσεις',
+
 );
