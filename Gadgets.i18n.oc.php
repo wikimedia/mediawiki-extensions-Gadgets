c91b843c028e48307050de590e4ac7d29596bb81
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-15 18:08:32 +0000

    Localisation updates from Betawiki. * Fixes and additions to 28 extensions for ast, bg, gd, hsb, fj, la, nl, oc, stq

--- Gadgets.i18n.oc.php
+++ Gadgets.i18n.oc.php
@@ -10,8 +10,12 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
+/** Occitan (Occitan)
+ * @author Cedric31
+ */
 $messages = array(
-	'gadgets-prefstext' => 'Çai jos apareis una lista de gadgets que podètz activar per vòstre compte. Fan apèl a JavaScript, deu doncas èsser activat per vòstre navegaire Web. An pas cap d\'incidéncia sus aquesta pagina de preferéncias. E mai, son generalament desvolopats e mantenguts sus aqueste wiki. Los administrators pòdon modificar los gadgets en passant per [[MediaWiki:Gadgets-definition]] e [[Special:Gadgets]].',
-	'gadgets-pagetext' => 'Çai jos apareis una lista de gadgets que los utilizaires pòdon activar dins lor pagina de preferéncias, coma definit dins \'\'[[MediaWiki:Gadgets-definition]]\'\'. Aqueste susvòl dona un accès rapid a las paginas de messatges del sistèma que definisson cada descripcion e cada còde dels gadgets.',
-	'gadgets-uses' => 'Utiliza',
+	'gadgets-prefstext' => "Çaijós apareis una lista de gadgets que podètz activar per vòstre compte. Fan apèl a JavaScript, deu doncas èsser activat per vòstre navegaire Web. An pas cap d'incidéncia sus aquesta pagina de preferéncias. E mai, son generalament desvolopats e mantenguts sus aqueste wiki. Los administrators pòdon modificar los gadgets en passant per [[MediaWiki:Gadgets-definition]] e [[Special:Gadgets]].",
+	'gadgets-pagetext'  => "Çaijós apareis una lista de gadgets que los utilizaires pòdon activar dins lor pagina de preferéncias, coma definit dins ''[[MediaWiki:Gadgets-definition]]''. Aqueste susvòl dona un accès rapid a las paginas de messatges del sistèma que definisson cada descripcion e cada còde dels gadgets.",
+	'gadgets-uses'      => 'Utiliza',
+
 );
