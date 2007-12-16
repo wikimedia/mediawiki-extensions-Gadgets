1706ca63d241015c51e26baed1eb9043583698de
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-16 10:00:20 +0000

    Localisation updates from Betawiki. * Fixes and additions to 48 extensions for ast, el, fi, fr, la, loz, no, oc, sq, stq, sv

---
+++ Gadgets.i18n.sv.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Swedish (Svenska)
+ * @author Lejonel
+ */
+$messages = array(
+	'gadgets-prefstext' => 'Härunder är en lista över tilläggsfunktioner ("gadgets") som du kan aktivera för ditt konto.
+De flesta funktionerna är baserade på JavaScript, så du måste ha JavaScript aktiverat i din webbläsare för att de ska fungera.
+Notera att de här tilläggsfunktionerna inte kommer ha någon effekt den här inställningssidan.
+
+Notera också att dessa "gadgets" inte är en del av MediaWiki-programvaran. De är för det mesta utvecklade och underhållna av användare på den här wikin. Lokala administratörer kan redigera de tillgängliga funktionerna med hjälp av [[MediaWiki:Gadgets-definition]] och [[Special:Gadgets]].',
+	'gadgets-pagetext'  => 'Härunder finns en lista över "gadgets" som användare kan aktivera i sina inställningar. Listan är definierad av [[MediaWiki:Gadgets-definition]].
+Den här översikten ger enkel åtkomst till de systemmeddelanden som definerar beskrivningarna och koden för alla "gadgets".',
+	'gadgets-uses'      => 'Använder',
+);
