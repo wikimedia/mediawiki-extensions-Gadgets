f025001b45ae57c70547f20f2299dad918bfe09f
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-16 23:54:39 +0000

    Localisation updates from Betawiki (2007-12-17 0:15 CET). * Fixes and additions to 42 extensions for bg, br, el, gl, loz, nl, oc, stq

--- Gadgets.i18n.gl.php
+++ Gadgets.i18n.gl.php
@@ -10,15 +10,21 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
+/** Galician (Galego)
+ * @author Alma
+ * @author Xosé
+ */
 $messages = array(
-	'gadgets-prefs' => 'Trebellos',
+	'gadgets-prefs'     => 'Trebellos',
 	'gadgets-prefstext' => 'Embaixo hai unha lista de trebellos especiais que pode activar para a súa conta.
 A maioría destes trebellos baséanse en JavaScript, así que ten que ter o JavaScript activado no seu navegador para que funcionen.
 Teña en conta que estes trebellos non funcionarán nesta páxina de preferencias.
 
 Teña tamén en conta que estes trebellos especiais non son parte do software de MediaWiki e que os crean e manteñen
 os usuarios no seu wiki local. Os administradores locais poden editar os trebellos disponíbeis mediante [[MediaWiki:Gadgets-definition]] e [[Special:Gadgets]].',
-	'gadgets' => 'Trebellos',
-	'gadgets-title' => 'Trebellos',
-	'gadgets-uses' => 'Usos',
+	'gadgets'           => 'Trebellos',
+	'gadgets-title'     => 'Trebellos',
+	'gadgets-pagetext'  => 'Embaixo hai unha listaxe dos trebellos especiais que os usuarios poden habilitar nas súa páxina de preferencias, tal e como se define en [[MediaWiki:Gadgets-definition]].
+Este panorama xeral é de doado acceso ao sistema dos mensaxes das páxinas que define cada descripción e código dos trebellos.',
+	'gadgets-uses'      => 'Usos',
 );
