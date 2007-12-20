a2995d5d7c7c87990da721bf2de00f561c34ba0a
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-20 00:14:40 +0000

    Localisation updates from Betawiki (2007-12-20 0:49 CET). * Fixes and additions to 16 extensions for am, bg, cs, fr, hr, lb, nl, oc,

---
+++ Gadgets.i18n.am.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Amharic (አማርኛ)
+ * @author Codex Sinaiticus
+ */
+$messages = array(
+	'gadgets-prefs'     => 'ተጨማሪ መሣርያዎች',
+	'gadgets-prefstext' => 'ከዚህ ታች አንዳንድ ተጨማሪ መሣርያ ወይም መኪናነት በዝርዝር ሊገኝ ይችላል። እነዚህ በደንብ እንዲሠሩ በኮምፒውተርዎ ላይ ጃቫ-ስክሪፕት እንዲኖር አስፈላጊነት ነው። 
+
+የዚህ ዊኪ መጋቢዎች [[MediaWiki:Gadgets-definition]] 
+እና [[Special:Gadgets]] በመጠቀም አዲስ መሣርያ ሊጨምሩ ይቻላል።',
+	'gadgets'           => 'ተጨማሪ መሣርያዎች',
+	'gadgets-title'     => 'ተጨማሪ መሣርያዎች',
+	'gadgets-pagetext'  => 'ተጨማሪ መሣርያዎች ወይም መኪናዎች በየዊኪ ፕሮዤ የለያያሉ።
+
+ተጨማሪ መሣሪያዎች ለማግኘት፣ ወደ [[Special:Preferences|ምርጫዎች]] ይሂዱ።
+
+የዚህ ገጽ መራጃ በተለይ ለመጋቢዎችና አስተዳዳሪዎች ይጠቅማል።
+
+በዚህ {{SITENAME}} የሚገኙት ተቸማሪ መሣርያዎች እነኚህ ናቸው፦',
+	'gadgets-uses'      => 'የተጠቀመው ጃቫ-ስክሪፕት',
+);
