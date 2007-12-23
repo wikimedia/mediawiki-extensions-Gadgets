0b247571b884c022728bc6a14cb461131474a9b6
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-23 11:21:34 +0000

    Localisation updates for extensions from Betawiki (2007-12-23 11:21 CET)

---
+++ Gadgets.i18n.it.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Italian (Italiano)
+ * @author BrokenArrow
+ */
+$messages = array(
+	'gadgets-prefstext' => "Di seguito viene presentata una lista di accessori speciali (''gadget'') che è possibile abilitare per il proprio account.
+La maggior parte di questi accessori è basata su JavaScript, è quindi necessario abilitare JavaScript nel proprio browser perché funzionino correttamente. Si noti che gli accessori non hanno alcun effetto in questa pagina di preferenze.
+
+Inoltre, si noti che questi accessori speciali non sono compresi nel software MediaWiki e vengono di solito realizzati e gestiti dagli utenti di ciascun sito wiki. Gli amministratori del sito possono modificare la lista degli accessori disponibili tramite le pagine [[MediaWiki:Gadgets-definition]] e [[Speciale:Accessori]]",
+	'gadgets'           => 'Accessori',
+	'gadgets-title'     => 'Accessori',
+	'gadgets-pagetext'  => "Di seguito viene presentato un elenco di accessori (''gadget'') che gli utenti possono abilitare sulla propria pagina delle preferenze, seguendo le definizioni riportate in [[MediaWiki:Gadgets-definition]]. Questa panoramica fornisce un comodo meccanismo per accedere ai messaggi di sistema nei quali sono definiti la descrizione e il codice di ciascun accessorio.",
+	'gadgets-uses'      => 'Utilizza',
+);
