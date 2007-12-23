38bf801bb10e80cfb6013352d4577b788abcab6b
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-23 11:25:25 +0000

    Localisation updates for extensions from Betawiki (2007-12-23 12:22 CET)

---
+++ Gadgets.i18n.lb.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages = array(
+	'gadgets-uses' => 'Benotzt',
+);
