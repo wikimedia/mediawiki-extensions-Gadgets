89299c7de7c76fa862a07ac149dadc8ac43196eb
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-24 01:11:42 +0000

    Localisation updates for extensions from Betawiki (2007-12-24 01:40 CET)

---
+++ Gadgets.i18n.ru.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Russian (Русский)
+ * @author VasilievVV
+ */
+$messages = array(
+	'gadgets-prefs'     => 'Гаджеты',
+	'gadgets-prefstext' => 'Ниже приведён список специальных гаджетов, которые вы можете включить для своей учётной записи.
+Эти гаджеты преимущественно основны на JavaScript, поэтому вы должны включить JavaScript для того, чтобы они работали.
+Учтите, что эти гаджеты не работают на странице настроек.
+
+Также учтите, что эти гаджеты не являются частью MediaWiki, и обычно рарабатываются и обслуживаются
+участниками вики. Администраторы могут изменять список гаджетов с помощью [[MediaWiki:Gadgets-definition]] 
+и [[Служебная:Gadgets]].',
+	'gadgets'           => 'Гаджеты',
+	'gadgets-title'     => 'Гаджеты',
+	'gadgets-pagetext'  => 'Ниже приведён список гаджетов, которые могут быть включены на странице [[Special:Preferences|настроек]].
+Этот список позволяет легко просматривать сообщения, которые содержат определения гаджетов.',
+	'gadgets-uses'      => 'Использует',
+);
