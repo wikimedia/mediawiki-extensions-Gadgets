b24cf8cc9c2e6533f807d9bfab548fd137b5b9c2
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-28 18:02:28 +0000

    Localisation updates for extension messages from Betawiki (2007-12-28 18.55 CET)

---
+++ Gadgets.i18n.stq.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Seeltersk (Seeltersk)
+ * @author Pyt
+ */
+$messages = array(
+	'gadgets-prefs'     => 'Gadgets',
+	'gadgets-prefstext' => 'Lieste fon spezielle Gadgets do der foar älken Benutser aktivierd wäide konnen.
+Do Gadgets basierje maastens ap Javascript, deeruum mout Javascript in dän Browser aktivierd weese, uumdät jo funktionierje.
+Do Gadgets funktionierje oawers nit ap disse Siede mäd persöönelke Ienstaalengen.
+
+Buutendät is tou beoachtjen, dät disse Gadgets in Algemeenen nit Paat fon MediaWiki sunt, man maast fon
+Benutsere fon lokoale Wikis äntwikkeld un fersuurged wäide. Lokoale Wiki-Administratore konnen ju Lieste fon
+ferföichboare Gadgets uur do Sieden [[MediaWiki:Gadgets-definition]] un [[Special:Gadgets]] beoarbaidje.',
+	'gadgets'           => 'Gadgets',
+	'gadgets-title'     => 'Gadgets',
+);
