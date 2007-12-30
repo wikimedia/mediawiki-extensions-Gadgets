9c93539d3c44a2a8cf5c4b58007068a3b0b62d7a
Bertrand Grondin <grondin@users.mediawiki.org>
2007-12-30 09:52:10 +0000

    Localisation updates for extensions from Betawiki (02:00, 30 December 2007 (UTC))

--- Gadgets.i18n.pt.php
+++ Gadgets.i18n.pt.php
@@ -14,11 +14,14 @@
  * @author 555
  */
 $messages = array(
+	'gadgets-prefs'     => 'Gadgets',
 	'gadgets-prefstext' => 'Segue-se uma lista de "gadgets" que podem ser ativados em sua conta.
 Tais gadgets normalmente são baseados em JavaScript, o que faz com que seja necessário que o suporte a JavaScript esteja ativado em seu navegador para que os mesmos funcionem.
 Note que os gadgets não possuem efeito nesta página (a página de preferências).
 
 Note também que tais gadgets não são parte do software MediaWiki, geralmente sendo desenvolvidos e mantidos por usuários de sua wiki local. Administradores locais podem editar os gadgets disponíveis através da [[MediaWiki:Gadgets-definition]] e [[{{ns:special}}:Gadgets]].',
+	'gadgets'           => 'Gadgets',
+	'gadgets-title'     => 'Gadgets',
 	'gadgets-pagetext'  => 'Segue-se uma lista de "gadgets" definidos em [[MediaWiki:Gadgets-definition]], que podem ser ativados por utilizadores através de suas páginas de preferências.
 Esta visão geral proporciona um acesso fácil para as mensagens de sistema que definem as descrições e códigos de cada um dos gadgets.',
 	'gadgets-uses'      => 'Utiliza',
