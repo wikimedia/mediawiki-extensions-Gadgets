97a0d6d79d426e17735d799556fade7009856913
Siebrand Mazeland <siebrand@users.mediawiki.org>
2007-12-31 00:12:42 +0000

    Localisation updates for extension messages from Betawiki (2007-12-31 0:35 CET)

--- Gadgets.i18n.no.php
+++ Gadgets.i18n.no.php
@@ -10,15 +10,18 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
+/** Norwegian (‪Norsk (bokmål)‬)
+ * @author Jon Harald Søby
+ */
 $messages = array(
-	'gadgets-prefs' => 'Tilleggsutstyr',
-	'gadgets-prefstext' => 'Under er en liste over tilleggsutstyr du kan slå på for kontoen din.
-Dette tilleggsutstyret er for det meste basert på JavaScript, så du må ha dette slått på i nettleseren din for at de skal fungere.
-Merk at tilleggsutstyret ikke val ha noen innvirkning på denne innstillingssiden.
+	'gadgets-prefs'     => 'Tilleggsfunksjoner',
+	'gadgets-prefstext' => 'Under er en liste over tilleggsfunksjoner du kan slå på for kontoen din.
+Disse funksjonene er for det meste basert på JavaScript, så du må ha dette slått på i nettleseren din for at de skal fungere.
+Merk at funksjonene ikke vil ha noen innvirkning på denne innstillingssiden.
 
 Merk også at disse verktøyene ikke er del av MediaWiki-programvaren, og vanligvis utvikles og vedlikeholdes av brukere på den lokale wikien. Lokale administratorer kan redigere tilgjengelig verktøy ved å bruke [[MediaWiki:Gadgets-definition]] og [[Special:Gadgets]].',
-	'gadgets' => 'Tilleggsutstyr',
-	'gadgets-title' => 'Tilleggsutstyr',
-	'gadgets-pagetext' => 'Under er en liste over tilleggsutstyr brukere kan slå på på sin innstillingsside, som definert av [[MediaWiki:Gadgets-definition]]. Denne oversikten gir lett tilgang til systembeskjedsidene som definerer hvert verktøys beskrivelse og kode.',
-	'gadgets-uses' => 'Bruk',
+	'gadgets'           => 'Tilleggsfunksjoner',
+	'gadgets-title'     => 'Tilleggsfunksjoner',
+	'gadgets-pagetext'  => 'Under er en liste over tilleggsfunksjoner brukere kan slå på på sin innstillingsside, som definert av [[MediaWiki:Gadgets-definition]]. Denne oversikten gir lett tilgang til systembeskjedsidene som definerer hvert verktøys beskrivelse og kode.',
+	'gadgets-uses'      => 'Bruk',
 );
