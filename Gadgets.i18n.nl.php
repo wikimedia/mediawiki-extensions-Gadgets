ba844c160bf14d679b161087008c735b8ad74c21
Bertrand Grondin <grondin@users.mediawiki.org>
2008-01-01 17:27:27 +0000

    Localisation updates for extensions from Betawiki (2008-01-01 15:30 UTC)

--- Gadgets.i18n.nl.php
+++ Gadgets.i18n.nl.php
@@ -11,20 +11,21 @@
  */
 
 /** Dutch (Nederlands)
+ * @author SPQRobin
  * @author Siebrand
  */
 $messages = array(
-	'gadgets-prefs'     => 'Extensies',
-	'gadgets-prefstext' => 'Hieronder staan de speciale extensies die u voor uw gebruiker kunt inschakelen.
-De extensies zijn voornamelijk gebaseerd op JavaScript, dus JavaScript moet voor uw browser zijn ingeschakeld om ze te laten werken.
-De extensies hebben geen invloed op deze pagina met voorkeuren.
+	'gadgets-prefs'     => 'Gadgets',
+	'gadgets-prefstext' => 'Hieronder staan de speciale gadgets die u voor uw gebruiker kunt inschakelen.
+De gadgets zijn voornamelijk gebaseerd op JavaScript, dus JavaScript moet voor uw browser zijn ingeschakeld om ze te laten werken.
+De gadgets hebben geen invloed op deze pagina met voorkeuren.
 
-Deze speciale extensies zijn geen onderdeel van de MediaWiki-software, en ze worden meestal ontwikkeld en onderhouden
-door gebruikers van uw lokale wiki. Lokale beheerders kunnen de beschikbare extensies aangeven in [[MediaWiki:Gadgets-definition]] 
+Deze speciale gadgets zijn ook geen onderdeel van de MediaWiki-software, en ze worden meestal ontwikkeld en onderhouden
+door gebruikers van uw lokale wiki. Lokale beheerders kunnen de beschikbare gadgets aangeven in [[MediaWiki:Gadgets-definition]] 
 en [[Special:Gadgets]].',
-	'gadgets'           => 'Extensies',
-	'gadgets-title'     => 'Extensies',
-	'gadgets-pagetext'  => 'Hieronder staan de speciale extensies die gebruikers kunnen inschakelen via hun voorkeuren, zoals ingesteld in [[MediaWiki:Gadgets-definition]].
-Dit overzicht biedt eenvoudige toegang tot de systeemtekstpagina waar de beschrijving en de programmacode van iedere extensie staat.',
+	'gadgets'           => 'Gadgets',
+	'gadgets-title'     => 'Gadgets',
+	'gadgets-pagetext'  => 'Hieronder staan de speciale gadgets die gebruikers kunnen inschakelen via hun voorkeuren, zoals ingesteld in [[MediaWiki:Gadgets-definition]].
+Dit overzicht biedt eenvoudige toegang tot de systeemtekstpagina waar de beschrijving en de programmacode van iedere gadget staat.',
 	'gadgets-uses'      => 'Gebruikt',
 );
