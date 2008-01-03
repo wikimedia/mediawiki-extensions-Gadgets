f54b3dab5aa71a1ce419c9bc04d35ebd4544ef56
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-03 22:34:13 +0000

    Localisation updates for extension messages from Betawiki (2008-01-03 23:09 CET)

---
+++ Gadgets.i18n.frp.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Internationalisation file for the Gadgets extension
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Daniel Kinzler, brightbyte.de
+ * @copyright © 2007 Betawiki editors
+ * @licence GNU General Public Licence 2.0 or later
+ */
+
+/** Franco-Provençal (Arpetan)
+ * @author ChrisPtDe
+ */
+$messages = array(
+	'gadgets-prefs'     => 'Outils',
+	'gadgets-prefstext' => 'Ce-desot aparêt una lista d’outils que vos pouede activar por voutron compto.
+Font apèl a [[JavaScript]], dêt vêr étre activâ por voutron navigator Malyâjo.
+Ils ont gins d’enfluence sur ceta pâge de prèferences.
+
+Et pués, sont g·ènèralament dèvelopâs et mantegnus sur ceti vouiqui. Los administrators pôvont modifiar los outils en passent per [[MediaWiki:Gadgets-definition]] et [[Special:Gadgets]].',
+	'gadgets'           => 'Outils',
+	'gadgets-title'     => 'Outils',
+	'gadgets-pagetext'  => "Ce-desot aparêt una lista d’outils que los utilisators pôvont activar dens lor [[Special:Preferences|pâge de prèferences]], tâl que dèfeni dens ''[[MediaWiki:Gadgets-definition]]''.
+Ceti survôlo balye un accès rapido a les pâges de mèssâjos sistèmo que dèfenéssont châque dèscripcion et châque code des outils.",
+	'gadgets-uses'      => 'Utilise',
+);
