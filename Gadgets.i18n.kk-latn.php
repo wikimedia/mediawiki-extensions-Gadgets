e8e38c9a19897c03c2084a099ffbf8ace8cb54c1
Shinjiman <shinjiman@users.mediawiki.org>
2008-01-12 07:24:17 +0000

    * (bug 7971) Update Kazakh translations for extensions   Patch by AlefZet

--- Gadgets.i18n.kk-latn.php
+++ Gadgets.i18n.kk-latn.php
@@ -12,18 +12,18 @@
  */
 
 #for Special:Preferences
-$messages['gadgets-prefs']     = 'Qajet närseler';
-$messages['gadgets-prefstext'] = 'Tömende öz tirkelgiñizde qosa alatın arnawlı qajet närseler tizimi beriledi.
-Osı qajet närseler köbinese JavaScript ämirlerine negizdelinedi, sondıqtan bular jumıs istewi üşin şolğışıñızda JavaScript qosılğan bolwı kerek.
-Bul baptaw betine osı qajet närseler äser etpeýtiniñ eskeriñiz.
+$messages['gadgets-prefs']     = 'Qajet quraldar';
+$messages['gadgets-prefstext'] = 'Tömende öz tirkelgiñizde qosa alatın arnawlı qajet quraldar tizimi beriledi.
+Osı qajet quraldar köbinese JavaScript ämirlerine negizdelinedi, sondıqtan bular jumıs istewi üşin şolğışıñızda JavaScript qosılğan bolwı kerek.
+Bul baptaw betine osı qajet quraldar äser etpeýtiniñ eskeriñiz.
 
-Tağı da eskeriñiz: osı qajet närseler MediaWiki bağdarlamasınıñ böligi emes, jäne de bulardı jaýşılıqta jergilikti wïkïdiñ qatıswşıları damıtadı jäne qoştaýdı.
+Tağı da eskeriñiz: osı qajet quraldar MediaWiki bağdarlamasınıñ böligi emes, jäne de bulardı jaýşılıqta jergilikti wïkïdiñ qatıswşıları damıtadı jäne qoştaýdı.
 Jergilikti äkimşiler jetimdi qajet närse tizimin [[{{ns:mediawiki}}:Gadgets-definition]] jäne [[{{ns:special}}:Gadgets]] betteri arqılı
 öñdeý aladı.';
 
 #for Special:Gadgets
-$messages['gadgets']          = 'Qajet närseler';
-$messages['gadgets-title']    = 'Qajet närseler';
-$messages['gadgets-pagetext'] = 'Tömende arnawlı qajet närseler tizimi beriledi. [[{{ns:mediawiki}}:Gadgets-definition]] betinde anıqtalğan qajet närselerdi qatıswşılar öziniñ baptawında qosa aladı.
-Bul şolw beti arqılı ärbir qajet närseniñ sïpattaması men ämirin anıqtaýtın jüýe xabar betterine jeñil qatınaý alasız.';
+$messages['gadgets']          = 'Qajet quraldar';
+$messages['gadgets-title']    = 'Qajet quraldar';
+$messages['gadgets-pagetext'] = 'Tömende arnawlı qajet quraldar tizimi beriledi. [[{{ns:mediawiki}}:Gadgets-definition]] betinde anıqtalğan qajet quraldardı qatıswşılar öziniñ baptawında qosa aladı.
+Bul şolw beti arqılı ärbir qajet quraldıñ sïpattaması men ämirin anıqtaýtın jüýe xabar betterine jeñil qatınaý alasız.';
 $messages['gadgets-uses']     = 'Qoldanwdağılar';
