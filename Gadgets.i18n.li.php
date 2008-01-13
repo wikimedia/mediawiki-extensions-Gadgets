5a4249880b3d106c56c1049caa4c90b469fcef1e
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-13 11:46:37 +0000

    Localisation updates for extension messages from Betawiki (2008-01-13 12:23 CET)

--- Gadgets.i18n.li.php
+++ Gadgets.i18n.li.php
@@ -1,9 +1,17 @@
 <?php
-
 /** Limburgish (Limburgs)
  * @author Ooswesthoesbes
  */
 $messages = array(
-	'gadgets-prefs' => 'Biedènger',
-	'gadgets-uses'  => 'Gebroek',
+	'gadgets-prefs'     => 'Biedènger',
+	'gadgets-prefstext' => 'Hiej ónger staon de speciaal oetbreijinger diet geer veur uche gebroeker kintj insjakele.
+De oetbreijinger zeen veurnamelik gebaseerdj op JavaScript, dus JavaScript mót veur uche browser ingesjakledj zeen óm die te laote wèrke.
+De oetbreijinge höbbe gein invloedj op dees pazjena mit veurkeure.
+
+Dees speciaal oetbreijinge zeen ouch gein óngerdeil van de MediaWiki-sofwaer en die mótte meistal óntwikkeldj en óngerhaoje waere door gebroekers van uche wiki. Beheerders kinne de besjikbare oetbreijinge aangaeve in [[MediaWiki:Gadgets-definition]] en [[Special:Gadgets]].',
+	'gadgets'           => 'Oetbreijinger',
+	'gadgets-title'     => 'Oetbreijinger',
+	'gadgets-pagetext'  => 'Hiej ónger staon de speciaal oetbreijinger die gebroekers kinne insjakele via häöre veurkeure wie ingesteldj is in [[MediaWiki:Gadgets-definition]].
+Dit euverzich bi-jtj uch einvoudige toegank toet de systeemtekspazjena wo de besjrieving en de programmacode van edere oetbreijing steit.',
+	'gadgets-uses'      => 'Gebroek',
 );
--- Gadgets.i18n.pl.php
+++ Gadgets.i18n.pl.php
@@ -10,11 +10,17 @@
  * @licence GNU General Public Licence 2.0 or later
  */
 
+/** Polish (Polski)
+ * @author Derbeth
+ * @author Sp5uhe
+ */
 $messages = array(
-	'gadgets-prefs' => 'Gadżety',
-	'gadgets-prefstext' => 'Poniżej znajduje się lista specjalnych gadżetów, które możesz włączyć dla swojego konta. Te gadżety opierają się w większości na JavaScripcie, więc by działały musisz mieć w swojej przeglądarce włączony JavaScript. Pamiętaj, że gadżety nie będą miały wpływu na tę stronę preferencji. Oprócz tego, nie są one częścią oprogramowania MediaWiki i zazwyczaj są tworzone przez użytkowników tego wiki. Lokalni administratorzy mogą edytować dostępne gadżety używając stron [[MediaWiki:Gadgets-definition]] i [[Special:Gadgets]].',
-	'gadgets' => 'Gadżety',
-	'gadgets-title' => 'Gadżety',
-	'gadgets-pagetext' => 'Poniżej znajduje się lista specjalnych gadżetów, które użytkownicy mogą włączyć na swojej stronie preferencji, jak zdefiniowano to na stronie [[MediaWiki:Gadgets-definition]]. Ten przegląd daje łatwy dostęp do komunikatów systemu, które definiują opis i kod każdego z gadżetów.',
-	'gadgets-uses' => 'Użycie',
+	'gadgets-prefs'     => 'Gadżety',
+	'gadgets-prefstext' => 'Poniżej znajduje się lista specjalnych gadżetów, które możesz włączyć dla swojego konta. Gadżety opierają się w większości na JavaScripcie, więc by działały musisz mieć w swojej przeglądarce włączony JavaScript. Pamiętaj, że nie będą one miały wpływu na tę stronę preferencji.
+
+Gadżety nie są częścią oprogramowania MediaWiki i najprawdopodobniej zostały stworzone przez użytkowników tego wiki. Lokalni administratorzy mogą edytować dostępne gadżety używając stron [[MediaWiki:Gadgets-definition]] i [[Special:Gadgets]].',
+	'gadgets'           => 'Gadżety',
+	'gadgets-title'     => 'Gadżety',
+	'gadgets-pagetext'  => 'Poniżej znajduje się lista specjalnych gadżetów, które użytkownicy mogą włączyć na swojej stronie preferencji, jak zdefiniowano to na stronie [[MediaWiki:Gadgets-definition]]. Ten przegląd daje łatwy dostęp do komunikatów systemu, które definiują opis i kod każdego z gadżetów.',
+	'gadgets-uses'      => 'Użycie',
 );
