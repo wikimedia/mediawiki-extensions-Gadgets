bb6d0d468b722cfb20e833c35f505c49f6b102ba
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-01-31 23:19:41 +0000

    Localisation updates for extension messages from Betawiki

---
+++ Gadgets.i18n.nn.php
@@ -0,0 +1,13 @@
+<?php
+/** Norwegian Nynorsk (‪Norsk (nynorsk)‬)
+ * @author Eirik
+ */
+$messages = array(
+	'gadgets-prefs'    => 'Tilleggsfunksjonar',
+	'gadgets'          => 'Tilleggsfunksjonar',
+	'gadgets-title'    => 'Tilleggsfunksjonar',
+	'gadgets-pagetext' => 'Under finn du ei liste over tilleggsfunksjonar som brukarane kan slå på på innstillingssidene sine, som oppgjeve i [[MediaWiki:Gadgets-definition]].
+Dette oversynet gjev enkel tilgang til systemmeldingssidene som inneheld skildringa og koden til kvar enkelt tilleggsfunksjon.',
+	'gadgets-uses'     => 'Brukar',
+);
+
