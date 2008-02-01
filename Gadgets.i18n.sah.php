d6dce7164d7511e69567749a473b91a3cd6193b3
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-01 10:17:29 +0000

    Localisation updates for extension messages from Betawiki (2008-02-01 11:04 CET)

---
+++ Gadgets.i18n.sah.php
@@ -0,0 +1,10 @@
+<?php
+/** Yakut (Саха тыла)
+ * @author HalanTul
+ */
+$messages = array(
+	'gadgets-prefs' => 'Гаджеттар',
+	'gadgets'       => 'Гаджеттар',
+	'gadgets-title' => 'Гаджеттар',
+);
+
