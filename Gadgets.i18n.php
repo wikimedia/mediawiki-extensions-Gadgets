15f00b1f84577a8e8416394e884dfd470e7b362b
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-02-16 16:12:44 +0000

    Localisation updates for extension messages from Betawiki (2008-02-16 16:34 CET)

--- Gadgets.i18n.php
+++ Gadgets.i18n.php
@@ -691,7 +691,7 @@ $messages['sah'] = array(
  * @author Helix84
  */
 $messages['sk'] = array(
-	'gadgets-desc'      => 'Umožňuje používateľovi vybrať JavaScriptové nástroje v [[Special:Preferences|nastaveniach]]',
+	'gadgets-desc'      => 'Umožňuje používateľovi vybrať [[Special:Gadgets|CSS a JavaScriptové nástroje]] vo svojich [[Special:Preferences|nastaveniach]]',
 	'gadgets-prefs'     => 'Nástroje',
 	'gadgets-prefstext' => 'Dolu je zoznam špeciálych nástrojov, ktoré môžete zapnúť v rámci svojho účtu. Tieto nástroje sú zväčša založené na JavaScripte, takže aby fungovali, musíte mať v prehliadači zapnutý JavaScript. Nástroje nemajú vplyv na túto stránku nastavení. Tiež majte na pamäti, že tieto nástroje nie sú súčasťou MediaWiki a zvyčajne ich vyvíjajú a udržiavajú používatelia vašej lokálnej wiki. Lokálni správcovia môžu upraviť zoznam dostupných nástrojov na [[MediaWiki:Gadgets-definition]] a [[Special:Gadgets]].',
 	'gadgets'           => 'Nástroje',
