e32e517b2651afd237db879a9f51c500054596c8
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-03-16 19:40:50 +0000

    Localisation updates for extensions messages from Betawiki (2008-03-16 20:06 CET)

--- Gadgets.i18n.php
+++ Gadgets.i18n.php
@@ -464,6 +464,20 @@ $messages['ja'] = array(
 	'gadgets-uses'      => '利用するファイル',
 );
 
+/** Jutish (Jysk)
+ * @author Huslåke
+ */
+$messages['jut'] = array(
+	'gadgets-prefs'     => 'Gøreter',
+	'gadgets-prefstext' => 'Nedenstående er en liste over de gadgets som du kan aktivere for din brugerkonto. Da disse gadgets hovedsageligt er baseret på JavaScript skal du slå JavaScript til i din browser for at få dem til at virke. Bemærk at disse gadgets ikke vil have nogen effekt på denne side (indstillinger).
+
+Bemærk også at disse specielle gadgets ikke er en del af MediaWiki-softwaren og at de typisk bliver vedligeholdt af brugere på din lokale wiki. Lokale administratorer kan redigere tilgængelige gadgets med [[MediaWiki:Gadgets-definition]] og [[Special:Gadgets]].',
+	'gadgets'           => 'Gøreter',
+	'gadgets-title'     => 'Gøreter',
+	'gadgets-pagetext'  => 'Nedenstående er en liste med de specielle gadgets som brugere kan aktivere i deres indstillinger som defineret i [[MediaWiki:Gadgets-definition]]. Denne oversigtsside giver simpel adgang til de systembeskeder som definerer hver gadgets beskrivelse og kode.',
+	'gadgets-uses'      => 'Brugere',
+);
+
 /** Kazakh (Arabic script) (‫قازاقشا (تٴوتە)‬) */
 $messages['kk-arab'] = array(
 	'gadgets-prefs'     => 'قاجەت قۇرالدار',
