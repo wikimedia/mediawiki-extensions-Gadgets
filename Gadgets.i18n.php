fb6bdb9e6e7becb9097aa031d0eccf417a8950e1
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-03-21 23:44:24 +0000

    Formatting of messages and message files. No content changes.

--- Gadgets.i18n.php
+++ Gadgets.i18n.php
@@ -19,14 +19,18 @@ $messages['en'] = array(
 
 	#for Special:Preferences
 	'gadgets-prefs'     => 'Gadgets',
-	'gadgets-prefstext' => 'Below is a list of special gadgets you can enable for your account. These gadgets are mostly based on JavaScript, so JavaScript has to be enabled in your browser for them to work. Note that these gadgets will have no effect on this preferences page.
+	'gadgets-prefstext' => 'Below is a list of special gadgets you can enable for your account.
+These gadgets are mostly based on JavaScript, so JavaScript has to be enabled in your browser for them to work.
+Note that these gadgets will have no effect on this preferences page.
 
-Also note that these special gadgets are not part of the MediaWiki software, and are usually developed and maintained by users on your local wiki. Local administrators can edit available gadgets using [[MediaWiki:Gadgets-definition]] and [[Special:Gadgets]].',
+Also note that these special gadgets are not part of the MediaWiki software, and are usually developed and maintained by users on your local wiki.
+Local administrators can edit available gadgets using [[MediaWiki:Gadgets-definition]] and [[Special:Gadgets]].',
 
 	#for Special:Gadgets
 	'gadgets'           => 'Gadgets',
 	'gadgets-title'     => 'Gadgets',
-	'gadgets-pagetext'  => "Below is a list of special gadgets users can enable on their preferences page, as defined by [[MediaWiki:Gadgets-definition]]. This overview provides easy access to the system message pages that define each gadget's description and code.",
+	'gadgets-pagetext'  => "Below is a list of special gadgets users can enable on their preferences page, as defined by [[MediaWiki:Gadgets-definition]].
+This overview provides easy access to the system message pages that define each gadget's description and code.",
 	'gadgets-uses'      => 'Uses',
 );
 
