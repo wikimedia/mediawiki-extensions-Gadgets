4f3af8eff3eed1200ce8d67ab3dfd916b507522b
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-07-13 13:41:02 +0000

    Localisation updates for extension special pages from Betawiki

--- Gadgets.i18n.alias.php
+++ Gadgets.i18n.alias.php
@@ -14,11 +14,26 @@ $aliases['en'] = array(
 	'Gadgets' => array( 'Gadgets' ),
 );
 
+/** Arabic (العربية) */
+$aliases['ar'] = array(
+	'Gadgets' => array( 'إضافات' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$aliases['arz'] = array(
+	'Gadgets' => array( 'إضافات' ),
+);
+
 /** German
  * @author Raimond Spekking
  */
 $aliases['de'] = array(
 	'Gadgets' => array( 'Gadgets' ),
+);
+
+/** Finnish (Suomi) */
+$aliases['fi'] = array(
+	'Gadgets' => array( 'Pienoisohjelmat' ),
 );
 
 /** Hebrew (עברית)
@@ -27,3 +42,24 @@ $aliases['de'] = array(
 $aliases['he'] = array(
 	'Gadgets' => array( 'סקריפטים' ),
 );
+
+/** Haitian (Kreyòl ayisyen) */
+$aliases['ht'] = array(
+	'Gadgets' => array( 'Gajèt' ),
+);
+
+/** Hungarian (Magyar) */
+$aliases['hu'] = array(
+	'Gadgets' => array( 'Segédeszközök' ),
+);
+
+/** Malay (Bahasa Melayu) */
+$aliases['ms'] = array(
+	'Gadgets' => array( 'Gajet' ),
+);
+
+/** Dutch (Nederlands) */
+$aliases['nl'] = array(
+	'Gadgets' => array( 'Extensies' ),
+);
+
