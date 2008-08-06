6fa9459d1ba2ae541859d29cb9f73fb94d4f61da
Siebrand Mazeland <siebrand@users.mediawiki.org>
2008-08-06 21:13:25 +0000

    Localisation updates for extension messages from Betawiki (2008-08-06 23:05 CEST)

--- Gadgets.i18n.php
+++ Gadgets.i18n.php
@@ -83,11 +83,12 @@ $messages['an'] = array(
 	'gadgets-prefstext' => "Contino ye una lista de trastes espezials que puede fer serbir en a suya cuenta.
 Como cuasi toz istos trastes son feitos en JavaScript, caldrá que tienga autibato JavaScript en o suyo nabegador ta que baigan bien. Pare cuenta que istos trastes no tendrán garra efeuto en ista pachina de preferenzias.
 
-Pare cuenta tamién que istos trastes espezials no fan parte d'o software MediaWiki, y que gosan estar desembolicatos y mantenitos por usuarios d'a suya wiki local. Os almenistradors locals pueden editar os trastes disponibles fendo serbir [[MediaWiki:Gadgets-definition]] y [[Special:Gadgets]].",
+Pare cuenta tamién que istos trastes espezials no fan parte d'o software MediaWiki, y que gosan estar desembolicatos y mantenitos por usuarios d'a suya wiki local. 
+Os almenistradors locals pueden editar os trastes disponibles en as pachinas de [[MediaWiki:Gadgets-definition|definizions]] y de [[Special:Gadgets|descripzions]].",
 	'gadgets'           => 'Trastes',
 	'gadgets-title'     => 'Trastes',
-	'gadgets-pagetext'  => "Contino ye una lista de trastes espezials que os usuarios pueden autibar en a suya pachina de preferenzias, como se define en [[MediaWiki:Gadgets-definition]].
-Ista lista premite ir rapedament t'as pachinas de mensaches d'o sistema que definen a descripzión y o codigo de cada traste.",
+	'gadgets-pagetext'  => "Contino ye una lista de trastes espezials que os usuarios pueden autibar en a suya [[Special:Preferences|pachina de preferenzias]], como se define en a pachina de [[MediaWiki:Gadgets-definition|definizions]].
+Ista lista premite ir fazilment t'as pachinas de mensaches d'o sistema que definen a descripzión y o codigo de cada traste.",
 	'gadgets-uses'      => 'Fa serbir',
 );
 
