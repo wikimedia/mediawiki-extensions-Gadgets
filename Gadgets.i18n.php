0bbd19c7188e573152a0e231f6153d71eb59feaf
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-01-25 23:10:35 +0000

    Localisation updates for extension messages from Betawiki (2009-01-25 23:00 UTC)

--- Gadgets.i18n.php
+++ Gadgets.i18n.php
@@ -954,16 +954,17 @@ Dit euverzich bi-jtj uch einvoudige toegank toet de systeemtekspazjena wo de bes
 );
 
 /** Lithuanian (Lietuvių)
+ * @author Homo
  * @author Vpovilaitis
  */
 $messages['lt'] = array(
 	'gadgets-desc' => 'Leidžia naudotojams pasirinkti savo [[Special:Gadgets|CSS ir JavaScript priemones]] jų [[Special:Preferences|nustatymuose]]',
 	'gadgets-prefs' => 'Priemonės',
 	'gadgets-prefstext' => 'Žemiau yra sąrašas specialių priemonių, kurias jūs galite įjungti naudojimui.
-Šios priemonės daugiausia yra sukurtos naudojant JavaScript, todėl JavaScript panaudojimas turi būti įjungtas jūsų brauseryje, kad jos veiktų.
-Atsiminkite, kad šios priemonės neturi itakos jūsų nustatymų puslapiui.
+Šios priemonės daugiausiai yra sukurtos naudojant JavaScript, todėl, kad jos veiktų, jūsų naršyklėje turi būti įjungtas JavaScript palaikymas.
+Atsiminkite, kad šios priemonės neturi įtakos jūsų nustatymų puslapiui.
 
-Taip pat žinokite, kad šios specialios priemonės nėra MediaWiki programinės įrangos dalis ir yra sukurtos ir palaikomos jūsų lokalaus viki vartotojų. Lokalūs administratoriai gali redaguoti suteikiamų specialių priemonių sąrašą, naudojant puslapius [[MediaWiki:Gadgets-definition|priemonių aprašymas]] ir [[Special:Gadgets|priemonės]].',
+Taip pat žinokite, kad šios specialios priemonės nėra MediaWiki programinės įrangos dalis ir yra sukurtos bei palaikomos vietinio vikiprojekto naudotojų. Vietiniai administratoriai gali redaguoti suteikiamų specialių priemonių sąrašą, naudodami puslapius [[MediaWiki:Gadgets-definition|priemonių aprašymas]] ir [[Special:Gadgets|priemonės]].',
 	'gadgets' => 'Priemonės',
 	'gadgets-title' => 'Priemonės',
 	'gadgets-pagetext' => 'Žemiau yra sąrašas specialių priemonių, kurias vartotojai gali įjungti savo nustatymo puslapyje, kurios apibrėžtos [[MediaWiki:Gadgets-definition|priemonių aprašyme]]. Ši apžvalga suteikia lengvą priėjimą prie sisteminių pranešimų puslapių, kuriuose apibrėžiamas kiekvienos priemonės trumpas aprašas ir kodas.',
