c772d66a6c6c56d28080d773affc713400d2f830
Roan Kattouw <catrope@users.mediawiki.org>
2011-07-21 00:10:51 +0000

    RL2: Document table fields

--- sql/gadgets.sql
+++ sql/gadgets.sql
@@ -1,8 +1,11 @@
 -- Add gadgets table
 
 CREATE TABLE /*_*/gadgets (
+	-- Name of gadget. Cannot be changed, ever.
 	gd_name varchar(255) binary NOT NULL PRIMARY KEY,
+	-- JSON blob with gadget properties. See (TODO: fill this) for documentation on the format
 	gd_blob mediumblob NOT NULL,
+	-- Whether or not this gadget is global (called 'shared' in the interface)
 	gd_global bool NOT NULL
 ) /*$wgDBTableOptions*/;
 
