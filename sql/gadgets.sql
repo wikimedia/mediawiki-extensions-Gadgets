7dc6694f3f17065397a4a9d6417262db1a1cc230
Roan Kattouw <catrope@users.mediawiki.org>
2011-08-06 08:16:04 +0000

    RL2: Schema changes: rename gd_global to gd_shared, and add gd_timestamp

--- sql/gadgets.sql
+++ sql/gadgets.sql
@@ -3,10 +3,13 @@
 CREATE TABLE /*_*/gadgets (
 	-- Name of gadget. Cannot be changed, ever.
 	gd_name varchar(255) binary NOT NULL PRIMARY KEY,
-	-- JSON blob with gadget properties. See (TODO: fill this) for documentation on the format
+	-- JSON blob with gadget properties. See Gadget::__construct() for documentation on the format
 	gd_blob mediumblob NOT NULL,
-	-- Whether or not this gadget is global (called 'shared' in the interface)
-	gd_global bool NOT NULL
+	-- Whether or not this gadget is allowed to be shared through a foreign repository
+	gd_shared bool NOT NULL,
+	-- The timestamp of when the metadata was last changed. Used for conflict detection and cache invalidation
+	gd_timestamp binary(14) NOT NULL
 ) /*$wgDBTableOptions*/;
 
-CREATE INDEX /*i*/gd_global_name ON /*_*/gadgets (gd_global, gd_name);
+CREATE INDEX /*i*/gd_shared_name ON /*_*/gadgets (gd_shared, gd_name);
+CREATE INDEX /*i*/gd_name_timestamp ON /*_*/gadgets (gd_name, gd_timestamp);
