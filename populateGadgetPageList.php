350e64537432776baf039e6e2e87fc54d84f40f3
Roan Kattouw <catrope@users.mediawiki.org>
2011-09-01 16:55:00 +0000

    RL2: Add maintenace script for populating the gadgetpagelist table

---
+++ populateGadgetPageList.php
@@ -0,0 +1,62 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = dirname( __FILE__ ) . '/../..';
+}
+require( "$IP/maintenance/Maintenance.php" );
+
+class PopulateGadgetPageList extends Maintenance {
+	const BATCH_SIZE = 100;
+	
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Populates the gadgetpagelist table";
+	}
+	
+	public function execute() {
+		$dbr = wfGetDB( DB_SLAVE );
+		$dbw = wfGetDB( DB_MASTER );
+		
+		$this->output( "Populating gadgetpagelist table ...\n" );
+		
+		$lastPageID = 0;
+		$processed = 0;
+		$written = 0;
+		while ( true ) {
+			// Grab a batch of pages from the page table
+			$res = $dbr->select( 'page',
+				array( 'page_id', 'page_namespace', 'page_title', 'page_is_redirect' ),
+				"page_id > $lastPageID", __METHOD__,
+				array( 'LIMIT' => self::BATCH_SIZE, 'ORDER BY' => 'page_id' )
+			);
+			if ( $dbr->numRows( $res ) == 0 ) {
+				// We've reached the end
+				break;
+			}
+			$processed += $dbr->numRows( $res );
+			
+			// Build gadgetpagelist rows
+			$gplRows = array();
+			foreach ( $res as $row ) {
+				$title = Title::newFromRow( $row );
+				if ( GadgetPageList::isGadgetPage( $title ) ) {
+					$gplRows[] = GadgetPageList::getRowForTitle( $title );
+				}
+				$lastPageID = intval( $row->page_id );
+			}
+			$dbr->freeResult( $res );
+			
+			// Insert the new rows
+			$dbw->insert( 'gadgetpagelist', $gplRows, __METHOD__, array( 'IGNORE' ) );
+			$written += count( $gplRows );
+			
+			$this->output( "... $processed pages processed, $written rows written, page_id $lastPageID\n" );
+		}
+		
+		$this->output( "Done\n" );
+	}
+}
+
+$maintClass = "PopulateGadgetPageList";
+require_once( DO_MAINTENANCE );
