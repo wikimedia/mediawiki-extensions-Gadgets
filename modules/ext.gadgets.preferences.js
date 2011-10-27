3d73986a1ecbb44415506fcce2ec649c7cbe6f0f
Roan Kattouw <catrope@users.mediawiki.org>
2011-10-27 13:02:42 +0000

    [RL2] Split out shared gadget prefs by repo, both in the data structure and on the form. This needs some love, still: repo names need i18n and the h1/h2 tags need better styling

--- modules/ext.gadgets.preferences.js
+++ modules/ext.gadgets.preferences.js
@@ -11,27 +11,32 @@
 	 * This hides the container td for shared gadgets preferences, reorders them by moving them
 	 * into a new td in the right order, updates their label texts, then replaces the old td with
 	 * the new one.
-	 * @param gadgetsByCategory {Object} Map of { categoryID: { gadgetID: gadgetDescription } }
-	 * @param categoryNames {Object} Map of { categoryID: categoryDescription }
+	 * @param gadgetsByCategory {Object} Map of { repo: { categoryID: { gadgetID: gadgetDescription } } }
+	 * @param categoryNames {Object} Map of { repo: { categoryID: categoryDescription } }
 	 */
 	function fixPreferenceForm( gadgetsByCategory, categoryNames ) {
+		// TODO i18n the repo names somehow
+		// TODO h1 and h2 need better styling
 		var 	$oldContainer = $( '#mw-prefsection-gadgetsshared' ).find( '.mw-input' ),
 			$newContainer = $( '<td>' ).addClass( 'mw-input' ),
 			$spinner = $oldContainer.closest( '.mw-gadgetsshared-item-unloaded' ),
-			category, gadget, $oldItem;
-		for ( category in gadgetsByCategory ) {
-			if ( category !== '' ) {
-				$newContainer.append( $( '<h1>' ).text( categoryNames[category] ) );
-			}
-			for ( gadget in gadgetsByCategory[category] ) {
-				// Find the item belonging to this gadget in $oldContainer
-				$oldItem = $oldContainer
-					.find( '#mw-input-wpgadgetsshared-' + gadget )
-					.closest( '.mw-htmlform-multiselect-item' );
-				// Update the label text
-				$oldItem.find( 'label' ).text( gadgetsByCategory[category][gadget] );
-				// Move the item from $oldContainer to $newContainer
-				$newContainer.append( $oldItem );
+			repo, category, gadget, $oldItem;
+		for ( repo in gadgetsByCategory ) {
+			$newContainer.append( $( '<h1>' ).text( repo ) );
+			for ( category in gadgetsByCategory[repo] ) {
+				if ( category !== '' ) {
+					$newContainer.append( $( '<h2>' ).text( categoryNames[repo][category] ) );
+				}
+				for ( gadget in gadgetsByCategory[repo][category] ) {
+					// Find the item belonging to this gadget in $oldContainer
+					$oldItem = $oldContainer
+						.find( '#mw-input-wpgadgetsshared-' + gadget )
+						.closest( '.mw-htmlform-multiselect-item' );
+					// Update the label text
+					$oldItem.find( 'label' ).text( gadgetsByCategory[repo][category][gadget] );
+					// Move the item from $oldContainer to $newContainer
+					$newContainer.append( $oldItem );
+				}
 			}
 		}
 		$oldContainer.replaceWith( $newContainer );
@@ -41,26 +46,47 @@
 
 	// Temporary testing data
 	var categoryNames = {
-		'foo': 'The Foreign Category of Foo'
+		'wiki2': {
+			'foo': 'The Foreign Category of Foo'
+		},
+		'wiki3': {
+			'foo': 'The Remote Category of Foo',
+			'bar': 'The Remote Category of Bar'
+		}
 	};
 	// TODO: Actually fetch this info using AJAX
 	// TODO: This structure allows cross-pollination of categories when multiple foreign repos are involved, do we want that?
 	// I guess probably not, because we wouldn't even know where to pull the category message from to begin with.
 	// Probably needs an extra level for the repo.
 	var gadgetsByCategory = {
-		'': {
-			'b': 'Gadget B',
-			'UTCLiveClock': 'A clock that displays UTC time blah blah blah'
+		'wiki2': {
+			'': {
+				'b': 'Gadget B',
+				'UTCLiveClock': 'A clock that displays UTC time blah blah blah'
+			},
+			'foo': {
+				'a': 'Gadget A'
+			}
 		},
-		'foo': {
-			'a': 'Gadget A'
+		'wiki3': {
+			'': {
+				'c': 'Gadget C',
+				'd': 'Gadget D'
+			},
+			'foo': {
+				'e': 'Gadget E'
+			},
+			'bar': {
+				'f': 'Gadget F',
+				'g': 'Gadget G'
+			}
 		}
 	};
 
 	$( function() {
 		// TODO make all of this nicer once we have AJAX
 		// Add spinner
-		$( '#mw-prefsection-gadgetsshared').find( '.mw-gadgetsshared-item-unloaded' ).addClass( 'mw-ajax-loader' );
+		$( '#mw-prefsection-gadgetsshared' ).find( '.mw-gadgetsshared-item-unloaded' ).addClass( 'mw-ajax-loader' );
 		// Simulate AJAX delay
 		setTimeout( function() { fixPreferenceForm( gadgetsByCategory, categoryNames ) }, 2000 );
 	} );
