1c02f54b495a8fa03720a0ebc16d9ca09748a061
Roan Kattouw <catrope@users.mediawiki.org>
2011-12-19 13:38:14 +0000

    [RL2] Moar tests
    * Inherit from MediaWikiTestCase and add @group Database. This allows us to safely write to the DB
    * Centralize the boilerplate data in its own function
    * The tests for the message functions add and delete MediaWiki: pages in the DB, and temporarily enable DB messages in MessageCache

--- tests/GadgetTest.php
+++ tests/GadgetTest.php
@@ -2,8 +2,9 @@
 
 /**
  * @group Gadgets
+ * @group Database
  */
-class GadgetTest extends PHPUnit_Framework_TestCase {
+class GadgetTest extends MediaWikiTestCase {
 	/**
 	 * @dataProvider provideValidatePropertiesArray
 	 */
@@ -48,9 +49,9 @@ class GadgetTest extends PHPUnit_Framework_TestCase {
 		);
 	}
 	
-	public function testGetters() {
-		$now = wfTimestampNow();
-		$data = array(
+	public static function getBoilerplateData() {
+		// Should be static or const or something, but PHP won't let us do that cause PHP sucks
+		return array(
 			'settings' => array(
 				'rights' => array( 'protect' ),
 				'default' => true,
@@ -65,7 +66,12 @@ class GadgetTest extends PHPUnit_Framework_TestCase {
 				'dependencies' => array( 'jquery.ui.button' ) 
 			)
 		);
-		$g = new Gadget( 'GadgetTest', LocalGadgetRepo::singleton(), $data, wfTimestampNow() );
+	}
+	
+	public function testGetters() {
+		$data = self::getBoilerplateData();
+		$now = wfTimestampNow();
+		$g = new Gadget( 'GadgetTest', LocalGadgetRepo::singleton(), $data, $now );
 		$this->assertEquals( $data, $g->getMetadata(), 'getMetadata' );
 		$this->assertEquals( FormatJson::encode( $data ), $g->getJSON(), 'getJSON' );
 		$this->assertEquals( 'GadgetTest', $g->getId(), 'getId' );
@@ -79,5 +85,103 @@ class GadgetTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( $data['module']['scripts'], $g->getScripts(), 'getScripts' );
 		$this->assertEquals( $data['module']['styles'], $g->getStyles(), 'getStyles' );
 		$this->assertEquals( $data['module']['dependencies'], $g->getDependencies(), 'getDependencies' );
+	}
+	
+	public function testMessageFunctions() {
+		global $wgLang;
+		
+		$g = new Gadget( 'gadgettest1', LocalGadgetRepo::singleton(), Gadget::getPropertiesBase(), wfTimestampNow() );
+		$this->assertEquals( 'gadget-gadgettest1-title', $g->getTitleMessageKey(), 'getTitleMessageKey' );
+		$this->assertEquals( 'gadget-gadgettest1-desc', $g->getDescriptionMessageKey(), 'getDescriptionMessageKey' );
+		
+		// Make sure the gadget-gadgettest1-{title,desc} messages exist
+		// In the test environment, the MessageCache is disabled because
+		// $wgUseDatabaseMessages is set to false. Temporarily enable it
+		// so we can write messages to the DB and use them.
+		MessageCache::singleton()->enable();
+		
+		$titleMsgTitle = Title::newFromText( 'MediaWiki:gadget-gadgettest1-title' );
+		$descMsgTitle = Title::newFromText( 'MediaWiki:gadget-gadgettest1-desc' );
+		if ( !$titleMsgTitle->exists() ) {
+			ParserTest::addArticle( $titleMsgTitle->getPrefixedText(), 'Gadget test 1', __LINE__ );
+		}
+		if ( !$descMsgTitle->exists() ) {
+			ParserTest::addArticle( $descMsgTitle->getPrefixedText(), 'Description of gadget test 1', __LINE__ );
+		}
+		$this->assertEquals( wfMessage( 'gadget-gadgettest1-title' )->plain(), $g->getTitleMessage(), 'getTitleMessage for existing message' );
+		$this->assertEquals( wfMessage( 'gadget-gadgettest1-desc' )->plain(), $g->getDescriptionMessage(), 'getDescriptionMessage for existing message' );
+		
+		$g = new Gadget( 'gadgettest2', LocalGadgetRepo::singleton(), Gadget::getPropertiesBase(), wfTimestampNow() );
+		$titleMsgTitle = Title::newFromText( 'MediaWiki:gadget-gadgettest2-title' );
+		$descMsgTitle = Title::newFromText( 'MediaWiki:gadget-gadgettest2-desc' );
+		if ( !$titleMsgTitle->exists() ) {
+			$page = WikiPage::factory( $titleMsgTitle );
+			$page->doDeleteArticle( 'Deleting to make way for test' );
+		}
+		if ( !$descMsgTitle->exists() ) {
+			$page = WikiPage::factory( $descMsgTitle );
+			$page->doDeleteArticle( 'Deleting to make way for test' );
+		}
+		$this->assertEquals( $wgLang->ucfirst( 'gadgettest2' ), $g->getTitleMessage(), 'getTitleMessage for nonexistent message' );
+		$this->assertEquals( '', $g->getDescriptionMessage(), 'getDescriptionMessage for nonexistent message' );
+		
+		MessageCache::singleton()->disable();
+		
+	}
+	
+	public function testGetModule() {
+		$data = self::getBoilerplateData();
+		$g = new Gadget( 'GadgetTest', LocalGadgetRepo::singleton(), $data, wfTimestampNow() );
+		$m = $g->getModule();
+		$pages = array(
+			'Gadget:Foo.js' => array( 'type' => 'script' ),
+			'Gadget:Bar.js' => array( 'type' => 'script' ),
+			'Gadget:Foo.css' => array( 'type' => 'style' ),
+		);
+		
+		$this->assertEquals( 'gadget.GadgetTest', $g->getModuleName(), 'getModuleName' );
+		$this->assertEquals( $g->getDependencies(), $m->getDependencies(), 'getDependencies' );
+		$this->assertEquals( $data['module']['messages'], $m->getMessages(), 'getMessages' );
+		$this->assertEquals( LocalGadgetRepo::singleton()->getSource(), $m->getSource(), 'getSource' );
+		$this->assertEquals( $pages, $m->getPages( ResourceLoaderContext::newDummyContext() ), 'getPages' );
+	}
+	
+	public function testIsEnabledForUser() {
+		$defaultOff = self::buildPropertiesArray( array( 'settings' => array( 'default' => false ) ) );
+		$defaultOn = self::buildPropertiesArray( array( 'settings' => array( 'default' => true ) ) );
+		$gOff = new Gadget( 'GadgetTestOffByDefault', LocalGadgetRepo::singleton(), $defaultOff, wfTimestampNow() );
+		$gOn = new Gadget( 'GadgetTestOnByDefault', LocalGadgetRepo::singleton(), $defaultOn, wfTimestampNow() );
+		$user = new User;
+		
+		$this->assertFalse( $gOff->isEnabledByDefault(), 'isEnabledByDefault for gOff' );
+		$this->assertTrue( $gOn->isEnabledByDefault(), 'isEnabledByDefault for gOn' );
+		$this->assertFalse( $gOff->isEnabledForUser( $user ), 'isEnabledForUser for gOff with default pref' );
+		$this->assertTrue( $gOn->isEnabledForUser( $user ), 'isEnabledForUser for gOn with default pref' );
+		
+		$user->setOption( 'gadget-GadgetTestOffByDefault', 0 );
+		$this->assertFalse( $gOff->isEnabledForUser( $user ), 'isEnabledForUser for gOff with pref off' );
+		$user->setOption( 'gadget-GadgetTestOffByDefault', 1 );
+		$this->assertTrue( $gOff->isEnabledForUser( $user ), 'isEnabledForUser for gOff with pref on' );
+		
+		$user->setOption( 'gadget-GadgetTestOnByDefault', 0 );
+		$this->assertFalse( $gOn->isEnabledForUser( $user ), 'isEnabledForUser for gOn with pref off' );
+		$user->setOption( 'gadget-GadgetTestOnByDefault', 1 );
+		$this->assertTrue( $gOn->isEnabledForUser( $user ), 'isEnabledForUser for gOn with pref on' );
+	}
+	
+	public function testIsAllowed() {
+		$data = self::buildPropertiesArray( array( 'settings' => array( 'rights' => array( 'foo', 'bar' ) ) ) );
+		$g = new Gadget( 'GadgetTest', LocalGadgetRepo::singleton(), $data, wfTimestampNow() );
+		$user = new User;
+		
+		// This is dirty, but I don't know how I would otherwise test this
+		$user->mRights = array();
+		$this->assertFalse( $g->isAllowed( $user ), 'user has no rights' );
+		$user->mRights = array( 'foo' );
+		$this->assertFalse( $g->isAllowed( $user ), 'user has foo right only' );
+		$user->mRights = array( 'bar' );
+		$this->assertFalse( $g->isAllowed( $user ), 'user has bar right only' );
+		$user->mRights = array( 'foo', 'bar' );
+		$this->assertTrue( $g->isAllowed( $user ), 'user has both foo and bar rights' );
 	}
 }
