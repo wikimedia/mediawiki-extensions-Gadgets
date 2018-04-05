441302fb5e3a0f7c47741e5ed3bb52725ba806b2
Kunal Mehta <legoktm@member.fsf.org>
2018-04-05 13:25:35 -0700

    build: Updating mediawiki/mediawiki-phan-config to 0.2.0
    Change-Id: I4d364243c33243aceeff9793bc3e69efd62ae491

--- tests/phan/config.php
+++ tests/phan/config.php
@@ -1,7 +1,6 @@
 <?php
 
 $cfg = require __DIR__ . '/../../vendor/mediawiki/mediawiki-phan-config/src/config.php';
-$cfg['suppress_issue_types'][] = 'PhanDeprecatedFunction';
 // Namespace constants
 $cfg['file_list'][] = 'Gadgets.namespaces.php';
 
