33ecce01c07484216bb9e4a6ceef25f98ba8564d
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2018-05-26 03:01:39 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 20.0.0
    Change-Id: I210883a100f2060e9bff0d5066896ce35b45a788

--- includes/MediaWikiGadgetsDefinitionRepo.php
+++ includes/MediaWikiGadgetsDefinitionRepo.php
@@ -120,7 +120,7 @@ class MediaWikiGadgetsDefinitionRepo extends GadgetRepo {
 	/**
 	 * Fetch list of gadgets and returns it as associative array of sections with gadgets
 	 * e.g. [ $name => $gadget1, etc. ]
-	 * @param string $forceNewText Injected text of MediaWiki:gadgets-definition [optional]
+	 * @param string|null $forceNewText Injected text of MediaWiki:gadgets-definition [optional]
 	 * @return array|bool
 	 */
 	public function fetchStructuredList( $forceNewText = null ) {
