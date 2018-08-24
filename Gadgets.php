7201979d4695392c8ec46d30002d514a2b3d38c2
Fomafix <fomafix@googlemail.com>
2018-08-24 18:25:45 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: Ie2713ad8156eee7c37d32a933214a1b60ded2e18

--- Gadgets.php
+++ Gadgets.php
@@ -6,10 +6,10 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	$wgMessagesDirs['Gadgets'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['GadgetsAlias'] = __DIR__ . '/Gadgets.alias.php';
 	$wgExtensionMessagesFiles['GadgetsNamespaces'] = __DIR__ . '/Gadgets.namespaces.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Gadgets extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return true;
 } else {
 	die( 'This version of the Gadgets extension requires MediaWiki 1.32+' );
