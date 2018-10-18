1acde38a6afaee62488096866c43cfc3680305c5
Aaron Schulz <aschulz@wikimedia.org>
2018-10-18 10:24:14 -0700

    Randomized APC gadget definition refreshes to avoid stampedes to memcached
    Bug: T203786
    Change-Id: If87e752f5cdc6bd216ad96486a86a40b2477b315

--- includes/MediaWikiGadgetsDefinitionRepo.php
+++ includes/MediaWikiGadgetsDefinitionRepo.php
@@ -80,8 +80,11 @@ class MediaWikiGadgetsDefinitionRepo extends GadgetRepo {
 
 		// (a) Check the tier 1 cache
 		$value = $t1Cache->get( $key );
+		// Randomize logical APC expiry to avoid stampedes
+		// somewhere between 7.0 and 10.0 (seconds)
+		$cutoffAge = mt_rand( 7 * 1e6, 10 * 1e6 ) / 1e6;
 		// Check if it passes a blind TTL check (avoids I/O)
-		if ( $value && ( microtime( true ) - $value['time'] ) < 10 ) {
+		if ( $value && ( microtime( true ) - $value['time'] ) < $cutoffAge ) {
 			$this->definitionCache = $value['gadgets']; // process cache
 			return $this->definitionCache;
 		}
