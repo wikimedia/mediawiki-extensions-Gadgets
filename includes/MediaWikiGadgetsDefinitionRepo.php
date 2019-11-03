7c133284678e78a4fc50c4db266625e0aafbb5c1
Umherirrender <umherirrender_de.wp@web.de>
2019-11-03 12:13:20 +0100

    build: Updating mediawiki/mediawiki-phan-config to 0.8.0
    Argument 1 ($min) is 7000000.0 but \mt_rand() takes int
    Argument 2 ($max) is 15000000.0 but \mt_rand() takes int
    
    Bug: T235049
    Change-Id: I2d7d0d6defd312dbed0de833678f3768fa3d7a46

--- includes/MediaWikiGadgetsDefinitionRepo.php
+++ includes/MediaWikiGadgetsDefinitionRepo.php
@@ -82,7 +82,7 @@ class MediaWikiGadgetsDefinitionRepo extends GadgetRepo {
 		$value = $t1Cache->get( $key );
 		// Randomize logical APC expiry to avoid stampedes
 		// somewhere between 7.0 and 15.0 (seconds)
-		$cutoffAge = mt_rand( 7 * 1e6, 15 * 1e6 ) / 1e6;
+		$cutoffAge = mt_rand( 7000000, 15000000 ) / 1000000;
 		// Check if it passes a blind TTL check (avoids I/O)
 		if ( $value && ( microtime( true ) - $value['time'] ) < $cutoffAge ) {
 			$this->definitionCache = $value['gadgets']; // process cache
