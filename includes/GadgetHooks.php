5ceabd721997dc3378f0d8acec13b2859ccc02cb
sahil <sahilgrewalhere2001@outlook.com>
2021-03-10 22:23:31 +0530

    Fixed typo "paramaters"
    Changed from paramaters to parameters in extensions / Gadgets
    
    Bug: T201491
    Change-Id: Ib5e85d64796d70cf58a068116e211e3c1c73470d

--- includes/GadgetHooks.php
+++ includes/GadgetHooks.php
@@ -76,7 +76,7 @@ class GadgetHooks {
 	 * Only run in versions of mediawiki begining 1.35; before 1.35, ::onPageContentSaveComplete
 	 * and ::onPageContentInsertComplete are used
 	 *
-	 * @note paramaters include classes not available before 1.35, so for those typehints
+	 * @note parameters include classes not available before 1.35, so for those typehints
 	 * are not used. The variable name reflects the class
 	 *
 	 * @param WikiPage $wikiPage
