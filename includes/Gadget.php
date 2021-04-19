662c63882cff86a5377d2335624d801edca7c434
Umherirrender <umherirrender_de.wp@web.de>
2021-04-20 01:13:27 +0200

    Reduce message parse in GadgetHooks::getPreferences
    Multiselect can build by message keys only
    
    This reverts fix for T32182, there is no way to handle the dir on each
    item/checkbox at the moment
    
    Bug: T58633
    Bug: T278650
    Depends-On: Ie983757081dc39f3685ba5b01b02bd124880e1af
    Change-Id: Iccd6202c443bd699aa3a911c8ba36a2b7bcdcfed

--- includes/Gadget.php
+++ includes/Gadget.php
@@ -138,17 +138,24 @@ class Gadget {
 	}
 
 	/**
+	 * @return string Message key
+	 */
+	public function getDescriptionMessageKey() {
+		return "gadget-{$this->getName()}";
+	}
+
+	/**
 	 * @return string Gadget description parsed into HTML
 	 */
 	public function getDescription() {
-		return wfMessage( "gadget-{$this->getName()}" )->parse();
+		return wfMessage( $this->getDescriptionMessageKey() )->parse();
 	}
 
 	/**
 	 * @return string Wikitext of gadget description
 	 */
 	public function getRawDescription() {
-		return wfMessage( "gadget-{$this->getName()}" )->plain();
+		return wfMessage( $this->getDescriptionMessageKey() )->plain();
 	}
 
 	/**
