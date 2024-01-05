18204a4b0db597fd9f522728c966cc81b06bbe09
Siddharth VP <siddharthvp@gmail.com>
2024-01-05 23:53:34 +0530

    Fix parsing logic when comments or hidden characters are present
    0c61a5a468ed9b6aafe939f60a60f9257db255e0 subtly affected the
    gadget definition parsing, causing issues with definitions containing
    HTML comments or certain kinds of whitespace characters. They would get
    saved to the pages array with a trailing whitespace but
    Gadget::getScripts() would only return pages strictly ending in `.js`.
    
    Trimming the title before in MediaWikiGadgetsDefinitionRepo fixes the
    issue.
    
    Bug: T354385
    Change-Id: Iacee432756006060217981a534434fd455285793

--- tests/phpunit/integration/MediaWikiGadgetsDefinitionRepoTest.php
+++ tests/phpunit/integration/MediaWikiGadgetsDefinitionRepoTest.php
@@ -18,6 +18,8 @@ class MediaWikiGadgetsDefinitionRepoTest extends MediaWikiIntegrationTestCase {
 * baz [rights=read] |baz.js
 ==keep-section2==
 * quux [rights=read] | quux.js
+* g1 [ResourceLoader | default | namespaces=2 | rights=editmyuserjs] | g1.js <!-- comment -->
+* g2 [ResourceLoader | default | namespaces=2 | rights=editmyuserjs] | <!-- comment --> g2.js
 EOT;
 		$this->editPage( 'MediaWiki:Gadgets-definition', $gadgetsDef );
 
@@ -25,10 +27,13 @@ EOT;
 		$repo = new MediaWikiGadgetsDefinitionRepo( $services->getMainWANObjectCache(),
 			$services->getRevisionLookup() );
 		$gadgets = $repo->fetchStructuredList();
-		$this->assertCount( 4, $gadgets );
+		$this->assertCount( 6, $gadgets );
 
 		$bar = $repo->getGadget( 'bar' );
 		$this->assertEquals( 'keep-section1', $bar->toArray()['category'] );
+
+		$this->assertEquals( [ 'MediaWiki:Gadget-g1.js' ], $repo->getGadget( 'g1' )->getScripts() );
+		$this->assertEquals( [ 'MediaWiki:Gadget-g2.js' ], $repo->getGadget( 'g2' )->getScripts() );
 	}
 
 	/**
