5ed81a79e26fd7c5ddc4bd80a5292db194f439e7
Martin Urbanec <martin.urbanec@wikimedia.cz>
2025-08-09 15:33:42 +0300

    Implement domain events interfaces
    This locks in the method signature and name in place,
    avoiding accidental mistakes. It also ensures other
    developers implementing domain events implement
    the right interface, as that is the current
    recommended method of using them.
    
    Last but not least, this actually makes it clear
    from the listeners that they are using a deprecated
    listener.
    
    Bug: T401527
    Change-Id: If5e78aeb601554d199419f96970bf044b274bcad

--- includes/EventIngress.php
+++ includes/EventIngress.php
@@ -4,14 +4,19 @@ namespace MediaWiki\Extension\Gadgets;
 
 use MediaWiki\DomainEvent\DomainEventIngress;
 use MediaWiki\Page\Event\PageDeletedEvent;
+use MediaWiki\Page\Event\PageDeletedListener;
 use MediaWiki\Page\Event\PageRevisionUpdatedEvent;
+use MediaWiki\Page\Event\PageRevisionUpdatedListener;
 use MediaWiki\Title\TitleValue;
 
 /**
  * Event subscriber acting as an ingress for relevant events emitted
  * by MediaWiki core.
  */
-class EventIngress extends DomainEventIngress {
+class EventIngress
+	extends DomainEventIngress
+	implements PageRevisionUpdatedListener, PageDeletedListener
+{
 	public function __construct(
 		private readonly GadgetRepo $gadgetRepo,
 	) {
